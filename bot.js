032a9000b66ce65becaa49c02976d886570a158d
Michael Schoen <ms778@mst.edu>
2016-06-13 18:32:06 -0500

    Testing

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("presence", (user1, user2) => {
-    console.log(user2.username);
+    bot.moveMember(user2, "190277585039654913")
 });
 
 bot.loginWithToken('MTkxNzQwNTY5MzQzNjIzMTY4.Cj-r0g.we0RnMxlRwi2e-Pdi4K6ENTEosU');
