f59b1087a43c81092e8ff58d3e770df7799e0772
Maneesh-Gona <mg9p8@mst.edu>
2016-06-13 18:22:54 -0500

    Got things printing to the console

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("presence", (user1, user2) => {
-    message.content === "Hi";
-    bot.reply(message, "Hey there!");
+    console.log(user2.username);
 });
 
 bot.loginWithToken('MTkxNzQwNTY5MzQzNjIzMTY4.Cj-r0g.we0RnMxlRwi2e-Pdi4K6ENTEosU');
