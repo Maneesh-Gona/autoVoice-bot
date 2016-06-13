d48aab62fd197273e135a85a7f67ad59038f5d4a
Michael Schoen <ms778@mst.edu>
2016-06-13 18:04:43 -0500

    testing

--- bot.js
+++ bot.js
@@ -4,14 +4,9 @@ var Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
-bot.on("message", function(message)
-{
-    if(message.content === "Hi") {
-        bot.reply(message, "Hey there!");
-    }
-    else if(message.content === "here comes dat boi!!!!!!") {
-        bot.reply(message, "o shit waddup!")
-    }
+bot.on("presence", (user1, user2) => {
+    message.content === "Hi";
+    bot.reply(message, "Hey there!");
 });
 
 bot.loginWithToken('MTkxNzQwNTY5MzQzNjIzMTY4.Cj-r0g.we0RnMxlRwi2e-Pdi4K6ENTEosU');
