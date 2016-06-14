5e0d183eed872bfd80669798fb11106f9548c275
Maneesh-Gona <mg9p8@mst.edu>
2016-06-13 19:21:41 -0500

    Waiting on presence fix

--- bot.js
+++ bot.js
@@ -5,7 +5,9 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("presence", (user1, user2) => {
-    bot.moveMember(user2, "190277585039654913")
+    console.log("Before");
+    bot.moveMember(user2, '190277585039654913');
+    console.log("After");
 });
 
 bot.loginWithToken('MTkxNzQwNTY5MzQzNjIzMTY4.Cj-r0g.we0RnMxlRwi2e-Pdi4K6ENTEosU');
