c7180da427c8e704aff3cbc9ee58d8a841e008e4
Michael Schoen <ms778@mst.edu>
2016-06-15 16:35:54 -0500

    Fixes equality

--- bot.js
+++ bot.js
@@ -6,13 +6,13 @@ var bot = new Discord.Client();
 
 bot.on("presence", (user1, user2) => {
     console.log('Test 1');
-    if((user2.game).equals('')) {
+    if(user2.game.equals(null)) {
         bot.moveMember(user2, '187325424743743490');
     }
-    else if((user2.game).equals('Overwatch')) {
+    else if(user2.game.name === 'Overwatch') {
         bot.moveMember(user2, '190277585039654913');
     }
-    else if((user2.game).equals('League of Legends')) {
+    else if(user2.game.name === 'League of Legends') {
         bot.moveMember(user2, '190277560695783424');
     }
     console.log('Test 2');
