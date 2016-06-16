e5c99793797cae8eed925fe6b19e839f91ba5156
Maneesh-Gona <mg9p8@mst.edu>
2016-06-15 23:30:14 -0500

    Fix comparison

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ var bot = new Discord.Client();
 
 
 bot.on("presence", (user1, user2) => {
-    console.log('Test 1');
-    if(user2.game.equals(null)) {
+    if(user2.game === null) {
         bot.moveMember(user2, '187325424743743490');
     }
     else if(user2.game.name === 'Overwatch') {
@@ -15,7 +14,6 @@ bot.on("presence", (user1, user2) => {
     else if(user2.game.name === 'League of Legends') {
         bot.moveMember(user2, '190277560695783424');
     }
-    console.log('Test 2');
 });
 
 bot.loginWithToken('MTkxNzQwNTY5MzQzNjIzMTY4.Cj-r0g.we0RnMxlRwi2e-Pdi4K6ENTEosU');
